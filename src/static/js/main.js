1668d1bc9e5c5dad2a49e87f223b947b46f0ae6f
kkgthb <44843658+kkgthb@users.noreply.github.com>
2021-02-10 13:30:55 -0600

    JavaScript that tells the DOM it's enabled

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,1 +1,9 @@
-// Just a comment for now
+// Helper:  returns first element selected - $('input[name="food"]') // Credit: https://twitter.com/wesbos/status/608341616173182977
+var $ = document.querySelector.bind(document);
+// Helper:  return array of selected elements - $$('img.dog') // Credit: https://twitter.com/wesbos/status/608341616173182977
+var $$ = document.querySelectorAll.bind(document);
+
+// To indicate that JavaScript is turned on, replace the "no-js" class on the "html" element with "js" instead
+(function (html) {
+  html.className = html.className.replace(/\bno-js\b/, "js");
+})(document.documentElement);
