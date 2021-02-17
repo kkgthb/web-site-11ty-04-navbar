196117797efe5597fc25fe90c9740146fafee485
kkgthb <44843658+kkgthb@users.noreply.github.com>
2021-02-17 09:30:57 -0600

    Fixed floaty long-text overflow JavaScript

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -33,6 +33,8 @@ function toggleNavDropdown(event) {
   if (event.type === "click") {
     // Turns out I don't need to add 32/13 (space/enter) "keypress" -- it just fights w/ browser treating it as "click"
     parent.classList.toggle("exposed");
+    // Also do some sizing on the "items" within
+    adjustDropdownItemsSize(parent);
   } else if (event.type === "keydown") {
     let code = event.charCode || event.keyCode;
     if (code === 27) {
@@ -65,6 +67,43 @@ function clearNavDropdownWhenFocusOutside(event) {
   }
 }
 
+// Helper:  getViewportWidth() // Credit: https://css-tricks.com/snippets/javascript/viewport-size-screen-resolution-mouse-postition/, Googled thanks to Jason Lengstorf, plus https://stackoverflow.com/questions/8339377/how-to-get-screen-width-without-minus-scrollbar
+function getViewportWidth() {
+  if (document.documentElement.clientWidth) {
+    return document.documentElement.clientWidth;
+  } else if (document.body && document.body.offsetWidth) {
+    return document.body.offsetWidth;
+  } else {
+    return 0;
+  }
+}
+
+// Helper:  set an element's width if it's overflowing
+function set_width_to_not_overflow_right(elem) {
+  let bounding = elem.getBoundingClientRect();
+  let screenwidth = getViewportWidth();
+  if (bounding.right > screenwidth) {
+    let subtract_me = bounding.right - screenwidth;
+    let current_width = bounding.right - bounding.left;
+    let new_width = current_width - subtract_me;
+    elem.style.minWidth = "auto";
+    elem.style.width = new_width + "px";
+  }
+}
+
+// Helper:  adjust all "dropdown items'" size for a given parent element
+function adjustDropdownItemsSize(parent) {
+  for (dropdown_items_elem of parent.querySelectorAll(
+    ".topnav__dropdown__items"
+  )) {
+    // First, reset them to their natural size
+    dropdown_items_elem.style.minWidth = "160px";
+    dropdown_items_elem.style.width = "auto";
+    // Then handle them if they're overflowing
+    set_width_to_not_overflow_right(dropdown_items_elem);
+  }
+}
+
 //
 //
 //
@@ -93,3 +132,8 @@ for (let btn of $$(".topnav__dropdown__button")) {
   btn.addEventListener("click", toggleNavDropdown);
   btn.addEventListener("keydown", toggleNavDropdown);
 }
+
+// Resize dropdown items if someone resizes a screen while a dropdown is exposed
+window.onresize = function () {
+  adjustDropdownItemsSize(document);
+};
